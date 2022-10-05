ff58adcc277a213df28d2914614fc6ce4d0d74b7
Dawid2806 <David-kaczmarek@gmx.de>
2022-10-05 12:06:27 +0200

    Minor: Tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,5 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Elooooo</h1>
-      <p>Hot dog</p>
-      <p>Hot dog</p>
-    </div>
-  );
+const Home = () => {
+  return <div></div>;
 };
 
 export default Home;
