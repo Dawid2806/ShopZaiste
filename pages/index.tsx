752cd87844c5d76efc699769bac5c0c7288e8025
Dawid2806 <David-kaczmarek@gmx.de>
2022-10-05 13:13:53 +0200

    chore: index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col bg-teal-100 min-h-screen">
+      <Header />
+      <main className="flex-grow max-w-2xl mx-auto grid p-6 gap-6 sm:grid-cols-2">
+        <img src="https://picsum.photos/id/1060/536/354" alt="Barista" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo eligendi
+          assumenda dolorum at velit placeat architecto aut? Magni, numquam
+          omnis!
+        </p>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
+// p-6 gap-6
