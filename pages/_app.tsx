f86e1589ff85860493d8831531b54125eab3a3cf
Dawid2806 <David-kaczmarek@gmx.de>
2022-10-14 17:31:41 +0200

    small changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import { QueryClientProvider } from "react-query";
+import { QueryClient } from "react-query";
 
+const client = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <QueryClientProvider client={client}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </QueryClientProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,26 @@
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import { Main } from "../components/Main";
+import { ProductDetails } from "../components/Product";
+
+const DATA = {
+  id: 1,
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo eligendi
+  assumenda dolorum at velit placeat architecto aut? Magni, numquam
+  omnis!`,
+  thumbailUrl: `https://picsum.photos/id/1060/536/354`,
+  thumbailAlt: `Barista parzy kawe`,
+  rating: 4.5,
+};
 
 const Home = () => {
   return (
-    <div className="flex flex-col bg-teal-100 min-h-screen">
-      <Header />
-      <main className="flex-grow max-w-2xl mx-auto grid p-6 gap-6 sm:grid-cols-2">
-        <img src="https://picsum.photos/id/1060/536/354" alt="Barista" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo eligendi
-          assumenda dolorum at velit placeat architecto aut? Magni, numquam
-          omnis!
-        </p>
-      </main>
-      <Footer />
+    <div className="flex flex-col bg-teal-100 h-screen ">
+      <Main>
+        <ProductDetails data={DATA} />
+      </Main>
     </div>
   );
 };
 
 export default Home;
-// p-6 gap-6
