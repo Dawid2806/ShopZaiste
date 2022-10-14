8cff4a35d6df56fa12a1830348ec5f02cf3fe615
Dawid2806 <David-kaczmarek@gmx.de>
2022-10-14 17:27:27 +0200

    feat:about page

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { Main } from "../components/Main";
+
+const about = () => {
+  return (
+    <div>
+      <Main>Cześć</Main>
+    </div>
+  );
+};
+
+export default about;
