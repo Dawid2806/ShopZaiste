7855b7289927eff6e91cce65c07d5d4cf35e6819
Dawid2806 <David-kaczmarek@gmx.de>
2022-10-18 20:37:46 +0200

    new productslist Component

--- components/Products/ProductsList.tsx
+++ components/Products/ProductsList.tsx
@@ -3,7 +3,7 @@ import { ProductsDetails } from "../../typs";
 
 type ProductListItem = Pick<
   ProductsDetails,
-  "title" | "thumbailAlt" | "thumbailUrl" | "id"
+  "title" | "thumbailAlt" | "thumbailUrl" | "id" | "price"
 >;
 
 interface ProductListItemProps {
@@ -14,15 +14,27 @@ interface ProductListItemProps {
 export const ProductListItem = ({ data, link }: ProductListItemProps) => {
   return (
     <>
-      <picture>
-        <img className="" src={data.thumbailUrl} alt={data.thumbailAlt} />
-      </picture>
-      <div>
-        <Link href={`${link}/${data.id}`}>
-          <a>
-            <h2 className="text-center mt-4">{data.title}</h2>
-          </a>
-        </Link>
+      <div className="group relative">
+        <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+          <img
+            src={data.thumbailUrl}
+            alt={data.thumbailAlt}
+            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+          />
+        </div>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm text-gray-700">
+              <Link href={`${link}/${data.id}`}>
+                <a>
+                  <span aria-hidden="true" className="absolute inset-0" />
+                  {data.title}
+                </a>
+              </Link>
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">{data.price} zl</p>
+          </div>
+        </div>
       </div>
     </>
   );
