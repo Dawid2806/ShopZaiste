2afb99f0a7b3184ce511c24a2c25d9c0d4da1258
Dawid2806 <David-kaczmarek@gmx.de>
2022-10-24 16:06:06 +0200

    Feat back button

---
+++ components/BackButton/BackButton.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+
+interface BackButtonProps {
+  link: string;
+}
+
+export const BackButton = ({ link }: BackButtonProps) => {
+  return (
+    <Link href={`${link}`}>
+      <button
+        type="button"
+        className="text-white ml-12 mt-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
+          />
+        </svg>
+      </button>
+    </Link>
+  );
+};
