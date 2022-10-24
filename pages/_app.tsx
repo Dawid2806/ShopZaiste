e4abbe27ad1ad5b2f2a1baf448d110720f0324d7
Dawid2806 <David-kaczmarek@gmx.de>
2022-10-24 17:51:05 +0200

    SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import type { AppProps } from "next/app";
 import { QueryClientProvider } from "react-query";
 import { QueryClient } from "react-query";
 import { Layout } from "../components/Layout.tsx/Layout";
-
+import { DefaultSeo } from "next-seo";
+import SEO from "./../next-seo.config";
 const client = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo {...SEO} />
       <QueryClientProvider client={client}>
         <Component {...pageProps} />
       </QueryClientProvider>
