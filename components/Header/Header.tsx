caf2cb3dda3cbfeacd1b78a843a264b22a94551c
Dawid2806 <David-kaczmarek@gmx.de>
2022-10-25 16:51:41 +0200

    add cursor pointer class

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,28 +8,16 @@ export const Header = () => {
       <nav className="bg-gray-700 text-white px-4 py-4  ">
         <ul className="flex flex-col md:flex-row md:gap-6 ">
           <Link href={"/"}>
-            <li className={router.pathname === "/" ? "selected" : ""}>
-              Główna
-            </li>
+            <li className="cursor-pointer">Główna</li>
           </Link>
           <Link href={"/about"}>
-            <li className={router.pathname === "/about" ? "selected" : ""}>
-              About
-            </li>
+            <li className="cursor-pointer">About</li>
           </Link>
           <Link href={"/products/1"}>
-            <li className={router.pathname === "/products" ? "selected" : ""}>
-              Products
-            </li>
+            <li className="cursor-pointer">Products</li>
           </Link>
           <Link href={"/products-csr?page=1"}>
-            <li
-              className={
-                router.pathname === "/products-csr?page={1}" ? "selected" : ""
-              }
-            >
-              Products-csr
-            </li>
+            <li className="cursor-pointer">Products-csr</li>
           </Link>
         </ul>
       </nav>
